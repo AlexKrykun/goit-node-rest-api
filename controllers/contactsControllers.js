3a6d2e7031c2862f6f516aeb54da6bbfdaed2146
Alex <140958369+AlexKrykun@users.noreply.github.com>
2024-05-07 12:02:44 +0300

    Complete_1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,12 +1,69 @@
 
-import contactsService from "../services/contactsServices.js";
+import HttpError from '../helpers/HttpError.js';
+import contactsService from '../services/contactsServices.js';
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (_, res, next) => {
+  try {
+    const allContacts = await contactsService.listContacts();
+    res.status(200).send(allContacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const oneContact = await contactsService.getContactById(id);
 
-export const deleteContact = (req, res) => {};
+    if (!oneContact) {
+      throw HttpError(404);
+    }
+    res.status(200).send(oneContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedContact = await contactsService.removeContact(id);
 
-export const updateContact = (req, res) => {};
+    if (!deletedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).send(deletedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const value = req.body;
+    const addContact = await contactsService.addContact(value);
+
+    return res.status(201).send(addContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const value = req.body;
+    const oneContact = await contactsService.getContactById(id);
+
+    if (!oneContact) {
+      throw HttpError(404);
+    }
+    const { name = oneContact.name, email = oneContact.email, phone = oneContact.phone } = value;
+    const updatedContact = await contactsService.updateContact(id, { name, email, phone });
+
+    return res.status(200).send(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
