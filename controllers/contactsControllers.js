5326234e424094ec137c8950f24c76e05dfec623
Alex <140958369+AlexKrykun@users.noreply.github.com>
2024-05-13 18:44:09 +0300

    Complete_4

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,11 @@
 
 import HttpError from '../helpers/HttpError.js';
-import contactsService from '../services/contactsServices.js';
+import Contact from '../models/contact.js';
 
 export const getAllContacts = async (_, res, next) => {
   try {
-    const allContacts = await contactsService.listContacts();
-    res.status(200).json(allContacts);
+    const allContacts = await Contact.find();
+    res.status(200).send(allContacts);
   } catch (error) {
     next(error);
   }
@@ -13,12 +13,11 @@ export const getAllContacts = async (_, res, next) => {
 
 export const getOneContact = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const oneContact = await contactsService.getContactById(id);
+    const { contactId } = req.params;
+    const oneContact = await Contact.findById(contactId);
 
-    if (!oneContact) {
-      throw HttpError(404);
-    }
+    if (!oneContact) throw HttpError(404);
+
     res.status(200).send(oneContact);
   } catch (error) {
     next(error);
@@ -27,12 +26,11 @@ export const getOneContact = async (req, res, next) => {
 
 export const deleteContact = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const deletedContact = await contactsService.removeContact(id);
+    const { contactId } = req.params;
+    const deletedContact = await Contact.findByIdAndDelete(contactId);
 
-    if (!deletedContact) {
-      throw HttpError(404);
-    }
+    if (!deletedContact) throw HttpError(404);
+
     res.status(200).send(deletedContact);
   } catch (error) {
     next(error);
@@ -41,8 +39,7 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const value = req.body;
-    const addContact = await contactsService.addContact(value);
+    const addContact = await Contact.create(req.body);
 
     return res.status(201).send(addContact);
   } catch (error) {
@@ -52,14 +49,14 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const { name, email, phone } = req.body;
-    const contact = await changeContact(id, { name, email, phone });
-    if (!contact) {
-      throw HttpError(404, "id not found");
-    }
-    res.json(contact);
-  } catch (err) {
-    next(err);
+    const { contactId } = req.params;
+
+    const updatedContact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+
+    if (!updatedContact) throw HttpError(404);
+
+    return res.status(200).send(updatedContact);
+  } catch (error) {
+    next(error);
   }
 };
